e52ae56a477197c2fdf9d974ab44209e30245d14
AbdulKhabir-Developer <116792547+AbdulKhabir-Developer@users.noreply.github.com>
2023-10-06 11:01:55 -0700

    Add files via upload

---
+++ Task 3/Js/script.js
@@ -0,0 +1,33 @@
+let input = document.getElementById('input');
+
+// select all through selector query
+let buttons = document.querySelectorAll('button');
+
+//take  null variable to store our result
+let string = "";
+
+//Array for buttons
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+
+        }
+        
+       
+    })
+})
